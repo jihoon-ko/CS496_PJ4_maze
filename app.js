3434e02942850869ee8bbcc03cc7ab73f839b03d
jihoon-ko <jihoonko@kaist.ac.kr>
2017-07-24 16:15:58 +0000

    new base

---
+++ app.js
@@ -0,0 +1,167 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+
+var app = express();
+var server = app.listen(3000);
+var io = require('socket.io').listen(server);
+var util = require('./lib/util');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+var players = [];
+var spectators = [];
+function getAllUsers() {
+  return players.concat(spectators);
+}
+var sockets = {};
+
+var maze = util.getNewMaze(6,6);
+
+io.on('connection', function (socket) {
+  console.log("Somebody connected!", socket.id);
+
+  //handle events from users
+  var currentUser = {
+    id: socket.id
+  };
+
+  socket.on('userRequiresGame', function (userName, userType) {
+    currentUser.name = userName;
+    currentUser.type = userType;
+    sockets[currentUser.id] = socket;
+
+    if(userType === 'player') {
+      //init player data: location, angle etc ...
+      players.push(currentUser);
+      socket.emit('serverAcceptsPlayer', maze, currentUser);
+    } else if(userType === 'spectator') {
+      //init spectator data
+      spectators.push(currentUser);
+      socket.emit('serverAcceptsSpectator', maze, currentUser);
+    } else {
+      console.log('user type error');
+    }
+  });
+
+  socket.on('playerSendsUpdates', function (data) {
+    //console.log("Hell Update");
+    currentUser.time = data.time;
+    currentUser.x = data.x;
+    currentUser.y = data.y;
+    currentUser.z = data.z;
+    currentUser.cx = data.cx;
+    currentUser.cy = data.cy;
+    currentUser.cz = data.cz;
+    currentUser.state = data.state;
+  });
+
+  socket.on('playerWins', function (potg) {
+    getAllUsers.forEach(function(user) {
+      user.result = (user.id === currentUser.id);
+    });
+    roundFinished(potg);
+  });
+
+  socket.on('disconnect', function (reason) {
+    console.log('disconnected', socket.id, reason);
+    if (currentUser.type === 'player') {
+      players = players.filter(function (p) {
+        return p.id !== currentUser.id;
+      });
+      console.log(players);
+    } else {
+      spectators = spectators.filter(function (s) {
+        return s.id !== currentUser.id;
+      });
+      console.log(spectators);
+    }
+
+  });
+
+  socket.on('disconnecting', function (reason) {
+    console.log('disconnecting', socket.id, reason);
+  });
+
+  socket.on('error', function (error) {
+    console.log(error);
+  });
+
+});
+function sendUpdates () {
+  //send updates to other users
+
+  var users = getAllUsers();
+  users.forEach(function (user) {
+    sockets[user.id].emit(
+      'serverSendsUpdates',
+      players.filter(function(p) { return user.id !== p.id; }));
+  });
+  //io.sockets.emit('serverSendsUpdates', players);
+}
+
+function roundFinished(potg) {
+  var users = getAllUsers();
+  users.forEach(function (user) {
+    sockets[user.id].emit('roundFinished', potg, user.result);
+  });
+  //potg 영상 재생 끝나는 시간 측정해서 대기한 후에: TODO
+  startNewRound();
+}
+
+
+function startNewRound() {
+  var users = getAllUsers();
+  maze = util.getNewMaze(6,6);
+  users.forEach(function (user) {
+    sockets[user.id].emit('serverStartsNewRound', maze);
+  });
+
+}
+
+function testEventEmit() {
+  if(players.length > 0) {
+    console.log('testEventEmit');
+    sockets[players[0].id].emit('testEvent');
+  }
+}
+setInterval(sendUpdates, 1000 / 30);
+
+
+
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ public/js/app.js
@@ -0,0 +1,78 @@
+
+var playerName;
+var playerNameInput = document.getElementById('playerNameInput');
+var socket;
+
+window.onload = function() {
+  var btnStart = document.getElementById('startButton');
+  var btnSpectate = document.getElementById('spectateButton');
+  var btnVR = document.getElementById('vrButton');
+  var nickErrorText = document.querySelector('#startMenu .input-error');
+
+  btnStart.onclick = function () {
+    // check if the nick is valid
+    if (validNick()) {
+      startGame('player');
+    } else {
+      nickErrorText.style.display = 'inline';
+    }
+  };
+
+  btnSpectate.onclick = function () {
+    startGame('spectator');
+  };
+
+  btnVR.onclick = function () {
+
+  };
+
+  playerNameInput.addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) { //KEY_ENTER
+      if (validNick()) {
+        startGame('player');
+      } else {
+        nickErrorText.style.display = 'inline';
+      }
+    }
+  });
+};
+
+// check if nick is valid alphanumeric characters (and underscores)
+function validNick() {
+  var regex = /^\w*$/;
+  console.log('Regex Test', regex.exec(playerNameInput.value));
+  return regex.exec(playerNameInput.value) !== null;
+}
+
+function startGame(type) {
+  playerName = playerNameInput.value.replace(/(<([^>]+)>)/ig, '').substring(0,25);
+  playerType = type;
+  document.getElementById('gameAreaWrapper').style.display = 'block';
+  document.getElementById('startMenuWrapper').style.display = 'none';
+  document.getElementById('myCanvas').style.display = 'block';
+
+  socket = io();
+  handleNetwork(socket);
+  //animloop();
+}
+
+// window.requestAnimFrame = (function(){
+//   return  window.requestAnimationFrame   ||
+//     window.webkitRequestAnimationFrame ||
+//     window.mozRequestAnimationFrame  ||
+//     function( callback ){
+//       window.setTimeout(callback, 1000 / 60);
+//     };
+// })();
+
+//적절히 교체해야..
+// function animloop(){
+//   requestAnimFrame(animloop);
+//   gameLoop();
+// }
+//
+// function gameLoop() {
+//   game.handleLogic();
+//   game.handleGraphics();
+// }
